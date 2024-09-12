43f1eb2bf83c425657fe11040719f2c48d48ece7
Pedro Romariz <pedroromariz1@gmail.com>
2024-09-12 13:12:38 +0100

    ultimos ajustes ao admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,14 +7,14 @@ class PostAdmin(admin.ModelAdmin):
 
     fields = ['author', 'title', 'text', 'created_date', 'published_date']
 
-    search_fields = ['author', 'title', 'text', 'created_date', 'published_date']
+    search_fields = ['author']
 
     list_filter = ['author', 'title' , 'published_date']
 
-    list_display = ['published_date']
+    list_display = ['published_date', 'author', 'title']
 
     list_editable = ['author', 'title']
 
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
